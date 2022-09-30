bf5b1a4d95156cf74fd868f43a3e6187a199d7bc
Kenneth Acerado <89882896+kenaceeee@users.noreply.github.com>
2022-09-30 21:51:52 +0800

    Add files via upload

---
+++ Demo1.php
@@ -0,0 +1,20 @@
+<html>
+    <body bgcolor = "#0000FF" >
+        <div class="main-div">
+            <?php
+                $full_name = "Jhon Kenneth Acerado";
+                $college_program = "BS Computer Engineering";
+                $year = "3rd";
+                $section = 301;
+
+                echo "Full name: $full_name";
+                echo "<br/>";
+                echo "College Program: $college_program";
+                echo "<br/>";
+                echo "Year and Section: $year - Block $section";
+                echo "<br/>";
+                echo "Today, I will learn PHP programming"
+            ?>
+        </div>
+    </body>
+</html>
